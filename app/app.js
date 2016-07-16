c019bc01d9f48ef208f7fb124bdbe660d2044da8
zhufengnodejs <zhufengnodejs@126.com>
2016-07-16 16:04:06 +0800

    实现登陆功能

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-angular.module('zhufengchat',['ngRoute','angularMoment']);
+angular.module('zhufengchat',['ngRoute','angularMoment'])
+.config(function($routeProvider){//配置路由
+    $routeProvider.when('/',{
+        templateUrl:'/pages/login.html',
+        controller:'LoginCtrl'
+    }).otherwise({
+        redirectTo:'/'
+    });
+});
