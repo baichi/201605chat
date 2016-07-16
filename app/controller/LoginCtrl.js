312c45d8821b65b6a45cf4161f524725da529804
zhufengnodejs <zhufengnodejs@126.com>
2016-07-16 16:19:11 +0800

    实现登陆功能

--- app/controller/LoginCtrl.js
+++ app/controller/LoginCtrl.js
@@ -1,3 +1,15 @@
-angular.module('zhufengchat').controller('LoginCtrl',function(){
+angular.module('zhufengchat').controller('LoginCtrl',function($scope,$http){
+    //实现用户的登陆 把用户填写的邮箱发送到后台
+    $scope.login = function(){
+        //$http $.ajax
+        $http({
+            url:'/user/login',
+            method:'POST',
+            data:{email:$scope.email}
+        }).success(function(data){
 
+        }).error(function(error){
+
+        });
+    }
 });
